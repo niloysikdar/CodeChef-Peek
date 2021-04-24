003ed87f1e69d9e75c583f891a7a39305e510e25
niloysikdar <niloysikdar30@gmail.com>
2021-04-24 21:14:02 +0530

    Pushed dirty code

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -23,85 +23,93 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         body: Container(
-          child: Column(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.asset(
-                  "assets/codechef.jpg",
-                  fit: BoxFit.fill,
-                  height: 120.0,
-                  width: 120.0,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.asset(
+                    "assets/codechef.jpg",
+                    fit: BoxFit.fill,
+                    height: 120.0,
+                    width: 120.0,
+                  ),
                 ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.black87,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
-                padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        child: TextField(
-                          controller: usernamecontroller,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                          ),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Enter a topic",
-                            hintStyle: TextStyle(
-                              color: Colors.white.withOpacity(0.6),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black87,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: TextField(
+                            controller: usernamecontroller,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            ),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Enter a topic",
+                              hintStyle: TextStyle(
+                                color: Colors.white.withOpacity(0.6),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: () async {
-                        try {
-                          http.Response response =
-                              await getResponse(usernamecontroller.text.trim());
-                          if (response.statusCode != 404) {
-                            try {
-                              UserModel userModel =
-                                  await getValidUser(response);
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => UserScreen(
-                                    userModel: userModel,
+                      GestureDetector(
+                        onTap: () async {
+                          try {
+                            http.Response response = await getResponse(
+                                usernamecontroller.text.trim());
+                            if (response.statusCode != 404) {
+                              try {
+                                UserModel userModel =
+                                    await getValidUser(response);
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => UserScreen(
+                                      userModel: userModel,
+                                    ),
                                   ),
-                                ),
-                              );
-                            } catch (e) {
-                              print(await getInvalidUser(response));
+                                );
+                              } catch (e) {
+                                print(await getInvalidUser(response));
+                              }
+                            } else {
+                              print("Getiing error");
                             }
-                          } else {
-                            print("Getiing error");
+                          } catch (e) {
+                            print("Check your internet connection");
                           }
-                        } catch (e) {
-                          print("Check your internet connection");
-                        }
-                      },
-                      child: Container(
-                        padding: EdgeInsets.only(left: 8.0),
-                        child: Icon(
-                          Icons.search_outlined,
-                          size: 36.0,
-                          color: Colors.white,
+                        },
+                        child: Container(
+                          padding: EdgeInsets.only(left: 8.0),
+                          child: Icon(
+                            Icons.search_outlined,
+                            size: 36.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                Container(
+                  height: 300.0,
+                  color: Colors.red,
+                ),
+              ],
+            ),
           ),
         ),
       ),
