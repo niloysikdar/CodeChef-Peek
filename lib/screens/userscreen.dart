0c53a1137f52c5baafb3828c5d894cebce3d8dc3
niloysikdar <niloysikdar30@gmail.com>
2021-04-27 00:27:40 +0530

    Updated background of circularprogress

--- lib/screens/userscreen.dart
+++ lib/screens/userscreen.dart
@@ -107,9 +107,26 @@ class _UserScreenState extends State<UserScreen> {
         ),
       ),
       circularStrokeCap: CircularStrokeCap.butt,
-      backgroundColor: kwhite,
+      backgroundColor: starcolorback(stars: stars),
       progressColor: starcolor(stars: stars),
     );
+  }
+
+  Color starcolorback({@required int stars}) {
+    if (stars == 1)
+      return Colors.grey[200];
+    else if (stars == 2)
+      return Colors.green[200];
+    else if (stars == 3)
+      return Colors.blue[200];
+    else if (stars == 4)
+      return Colors.purple[200];
+    else if (stars == 5)
+      return Colors.yellow[200];
+    else if (stars == 6)
+      return Colors.orange[200];
+    else
+      return Colors.red[200];
   }
 
   Color starcolor({@required int stars}) {
